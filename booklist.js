985499749ac45419f0aaa951c7307490d5e431a2
Student <user@chromebox-004.(none)>
2013-10-17 17:51:05 -0700

    events

--- booklist.js
+++ booklist.js
@@ -1,12 +1,25 @@
 var Booklist = function( arrayOfBooks ) {
     
     this.lastBook = null;
-    this.bookShelf = arrayOfBooks;
-    this.currentBook = this.bookShelf[0];
-
+    this.bookShelf = [];
     this.booksRead = 0;
     this.booksNotRead = 0;
  
+    this.addToDom = function(book){
+        var content = document.getElementById("container");
+        var p = document.createElement("p");
+        p.setAttribute("id",book.bookTitle);
+        p.className = "book";
+
+        p.innerHTML="<img src="+book.coverURL+" class=\"cover\" width=\"200\"><ul><li>Title: " + book.bookTitle + "</li><li>Author: " + book.author + "</li></ul>";
+
+        content.appendChild(p);
+
+        if (book.read){
+            showBookAsRead(book);
+        }
+    };
+
     this.add = function(book) {
         //add book to list
         this.bookShelf.push(book);
@@ -15,7 +28,14 @@ var Booklist = function( arrayOfBooks ) {
         }else{
             this.booksNotRead++;
         }
+        this.addToDom(book);
     };
+
+    for (var i = 0; i < arrayOfBooks.length; i++) {
+        this.add(arrayOfBooks[i]);
+    }
+
+    this.currentBook = this.bookShelf[0];
 
     this.finishCurrentBook = function() {
         if(this.currentBook){
@@ -25,7 +45,9 @@ var Booklist = function( arrayOfBooks ) {
             showBookAsRead(this.currentBook);
             this.booksNotRead--;
             this.lastBook = this.currentBook;
+            this.resetStyle();
             this.currentBook = this.nextBook;
+            styleCurrentBook(this.currentBook);
             this.nextBook = this.findNextBook();
         }else{
             console.log("You've read all the books!");
@@ -42,6 +64,12 @@ var Booklist = function( arrayOfBooks ) {
             }
         }
         return null;
+    };
+
+    this.resetStyle = function(){
+        var last = document.getElementById(this.lastBook.bookTitle);
+        console.log(last);
+        last.className = "book read";
     };
 
     this.nextBook = this.findNextBook();
@@ -72,30 +100,60 @@ function showBookAsRead(book){
     p.className = p.className + " read";
 }
 
+function styleCurrentBook(book) {
+    var p = document.getElementById(book.bookTitle);
+    p.className = p.className + " currentBook";
+}
+
+function addBook(e) {
+    /* get all the values of the form, 
+    make a new book object with those values,
+    call booklist.add() with the new book we just made */
+    bookTitle = document.getElementsByName("bookTitle")[0].value;
+    bookGenre = document.getElementsByName("genre")[0].value;
+    bookAuthor = document.getElementsByName("author")[0].value;
+    bookRead = document.getElementsByName("read")[0].value;
+    if (bookRead === "Yes") {
+        bookRead = true;
+    }else{
+        bookRead = false;
+    }
+    bookCover = document.getElementsByName("coverURL")[0].value;
+
+    var newBook = new Book(bookTitle, bookGenre, bookAuthor, bookRead, bookCover);
+    myBooklist.add(newBook);
+    
+    // e.preventDefault(); // browser - don't act!
+    // e.stopPropagation(); // bubbling - stop
+    // return false; // added for completeness
+}
+
 var gulliver = new Book("Gulliver's Travels", "Satire", "Johnathan Swift", false, "http://4.bp.blogspot.com/-4G4nwN7K7mA/TkWDW4J_08I/AAAAAAAAAmc/xegiKU2mqDs/s400/Gulliver%2527s+Travels+movie+pictures+image.jpg");
 var oliver = new Book("Oliver Twist", "Historical", "Charles Dickens", true, "http://www.mountainsoftravelphotos.com/ReferenceImages/Charles%20Dickins%20Oliver%20Twist%20Book.jpg");
 var twilight = new Book("Twilight", "Young Adult Fantasy", "Stephanie Meyers", false, "http://upload.wikimedia.org/wikipedia/en/1/1d/Twilightbook.jpg");
 var potter = new Book("Harry Potter", "Fantasy", "JK Rowling", false, "http://library.wheelock.edu/blog/wp-content/uploads/harrypotter.jpg");
 var iceAndFire = new Book("Game of Thrones", "Fantasy", "George R R Martin", true, "http://3.bp.blogspot.com/-Sc5_-__fCsg/Tl6N3BQ03CI/AAAAAAAABGk/fpGPt74Jqk8/s1600/Game+of+thrones+US+1.jpg");
 
-var books = [gulliver, iceAndFire, oliver, potter];
-
+var books = [gulliver, iceAndFire, oliver, potter, twilight];
 var myBooklist = new Booklist(books);
-//myBooklist.finishCurrentBook();
-myBooklist.add(twilight);
 
 var content = document.getElementById("container");
 
-for (var i=0; i<myBooklist.bookShelf.length; i++) {
-    var p = document.createElement("p");
-    p.setAttribute("id",myBooklist.bookShelf[i].bookTitle);
-    p.className = "book";
+// for (var i=0; i<myBooklist.bookShelf.length; i++) {
+//     var p = document.createElement("p");
+//     p.setAttribute("id",myBooklist.bookShelf[i].bookTitle);
+//     p.className = "book";
 
-    p.innerHTML="<img src="+myBooklist.bookShelf[i].coverURL+" class=\"cover\" width=\"200\"><ul><li>Title: " + myBooklist.bookShelf[i].bookTitle + "</li><li>Author: " + myBooklist.bookShelf[i].author + "</li></ul>";
-   
-    content.appendChild(p);
+//     p.innerHTML="<img src="+myBooklist.bookShelf[i].coverURL+" class=\"cover\" width=\"200\"><ul><li>Title: " + myBooklist.bookShelf[i].bookTitle + "</li><li>Author: " + myBooklist.bookShelf[i].author + "</li></ul>";
 
-    if (myBooklist.bookShelf[i].read){
-        showBookAsRead(myBooklist.bookShelf[i]);
-    }
-}
+//     content.appendChild(p);
+
+//     if (myBooklist.bookShelf[i].read){
+//         showBookAsRead(myBooklist.bookShelf[i]);
+//     }
+// }
+
+styleCurrentBook(myBooklist.currentBook);
+
+var submitButton = document.getElementById("submitBook");
+submitButton.addEventListener('click', addBook, false);
